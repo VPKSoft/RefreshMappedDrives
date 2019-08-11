6b516355a7a71578abc0182010689beae763b348
VPKSoft <vpksoft@vpksoft.net>
2019-08-11 18:21:22 +0300

    Initial commit.

---
+++ RefreshMappedDrives/FormMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace RefreshMappedDrives
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.lvDrives = new System.Windows.Forms.ListView();
+            this.colStateOk = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDriveLetter = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colUNCPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this._colOk = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imStateIcons = new System.Windows.Forms.ImageList(this.components);
+            this.tmRefresh = new System.Windows.Forms.Timer(this.components);
+            this.niTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsTray.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvDrives
+            // 
+            this.lvDrives.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvDrives.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colStateOk,
+            this.colDriveLetter,
+            this.colUNCPath,
+            this._colOk});
+            this.lvDrives.HideSelection = false;
+            this.lvDrives.Location = new System.Drawing.Point(12, 12);
+            this.lvDrives.Name = "lvDrives";
+            this.lvDrives.Size = new System.Drawing.Size(433, 117);
+            this.lvDrives.SmallImageList = this.imStateIcons;
+            this.lvDrives.StateImageList = this.imStateIcons;
+            this.lvDrives.TabIndex = 1;
+            this.lvDrives.UseCompatibleStateImageBehavior = false;
+            this.lvDrives.View = System.Windows.Forms.View.Details;
+            // 
+            // colStateOk
+            // 
+            this.colStateOk.Text = "Re-Map OK";
+            this.colStateOk.Width = 70;
+            // 
+            // colDriveLetter
+            // 
+            this.colDriveLetter.Text = "Drive Letter";
+            this.colDriveLetter.Width = 89;
+            // 
+            // colUNCPath
+            // 
+            this.colUNCPath.Text = "Network Path";
+            this.colUNCPath.Width = 247;
+            // 
+            // _colOk
+            // 
+            this._colOk.Width = 0;
+            // 
+            // imStateIcons
+            // 
+            this.imStateIcons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imStateIcons.ImageStream")));
+            this.imStateIcons.TransparentColor = System.Drawing.Color.Transparent;
+            this.imStateIcons.Images.SetKeyName(0, "Error.png");
+            this.imStateIcons.Images.SetKeyName(1, "OK.png");
+            // 
+            // tmRefresh
+            // 
+            this.tmRefresh.Interval = 500;
+            this.tmRefresh.Tick += new System.EventHandler(this.TmRefresh_Tick);
+            // 
+            // niTray
+            // 
+            this.niTray.ContextMenuStrip = this.cmsTray;
+            this.niTray.Icon = ((System.Drawing.Icon)(resources.GetObject("niTray.Icon")));
+            this.niTray.Text = "Refresh Mapped Drives [Copyright";
+            this.niTray.Visible = true;
+            // 
+            // cmsTray
+            // 
+            this.cmsTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.cmsTray.Name = "cmsTray";
+            this.cmsTray.Size = new System.Drawing.Size(94, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Image = global::RefreshMappedDrives.Properties.Resources.Exit;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 141);
+            this.Controls.Add(this.lvDrives);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Refresh Mapped Drives [Copyright © VPKSoft 2019]";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Shown += new System.EventHandler(this.FormMain_Shown);
+            this.cmsTray.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView lvDrives;
+        private System.Windows.Forms.ColumnHeader colStateOk;
+        private System.Windows.Forms.ColumnHeader colDriveLetter;
+        private System.Windows.Forms.ColumnHeader colUNCPath;
+        private System.Windows.Forms.ImageList imStateIcons;
+        private System.Windows.Forms.Timer tmRefresh;
+        private System.Windows.Forms.ColumnHeader _colOk;
+        private System.Windows.Forms.NotifyIcon niTray;
+        private System.Windows.Forms.ContextMenuStrip cmsTray;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ RefreshMappedDrives/FormMain.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Threading;
+
+namespace RefreshMappedDrives
+{
+    public partial class FormMain : Form
+    {
+        private int TryCountSeconds { get; }
+
+        private int CurrentCountMilliseconds { get; set; }
+
+        public FormMain()
+        {
+            InitializeComponent();
+            foreach (var arg in Environment.GetCommandLineArgs())
+            {
+                if (arg.StartsWith("--tryCount", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    try
+                    {
+                        TryCountSeconds = int.Parse(arg.Split('=')[0]);
+                    }
+                    catch
+                    {
+                        TryCountSeconds = 10;
+                    }
+                }
+            }
+        }
+
+        private void UpdateItemsToOk()
+        {
+            foreach (ListViewItem item in lvDrives.Items)
+            {
+                item.ImageIndex = 1;
+                item.SubItems[3].Text = true.ToString();
+            }
+        }
+
+        private ListViewItem GetExistingItem(string drive)
+        {
+            foreach (ListViewItem item in lvDrives.Items)
+            {
+                if (item.SubItems[1].Text == drive)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+        private bool ItemOk(ListViewItem item)
+        {
+            return bool.Parse(item.SubItems[3].Text);
+        }
+
+
+        private bool AllOk
+        {
+            get
+            {
+                bool result = true;
+                foreach (ListViewItem item in lvDrives.Items)
+                {
+                    result &= bool.Parse(item.SubItems[3].Text);
+                }
+
+                return result;
+            }
+        }
+
+        private void RefreshDrives()
+        {
+            try
+            {
+                UpdateItemsToOk();
+                using (Runspace runSpace = RunspaceFactory.CreateRunspace(RunspaceConfiguration.Create()))
+                {
+                    runSpace.Open();
+
+                    using (Pipeline pipeline = runSpace.CreatePipeline())
+                    {
+
+                        pipeline.Commands.AddScript(
+                            "Get-SmbMapping | where -property Status -Value Unavailable -EQ | select LocalPath,RemotePath");
+
+                        var results = pipeline.Invoke();
+
+                        foreach (var result in results)
+                        {
+                            var listViewItem = GetExistingItem(result.Properties["LocalPath"].Value.ToString()) == null
+                                ? new ListViewItem()
+                                : GetExistingItem(result.Properties["LocalPath"].Value.ToString());
+
+                            if (listViewItem.SubItems.Count > 1)
+                            {
+                                // handled already..
+                                continue;
+                            }
+
+                            listViewItem.ImageIndex = 0;
+                            listViewItem.SubItems.Clear();
+                            listViewItem.SubItems.Add(result.Properties["LocalPath"].Value.ToString());
+                            listViewItem.SubItems.Add(result.Properties["RemotePath"].Value.ToString());
+                            listViewItem.SubItems.Add(false.ToString());
+                            lvDrives.Items.Add(listViewItem);
+                        }
+                    }
+
+                    runSpace.Close();
+
+                    if (AllOk)
+                    {
+                        AllowClose = true;
+                        Close();
+                    }
+                }
+            }
+            catch
+            {
+                // ignored..
+            }
+        }
+
+        private void UpdateMappedDrives()
+        {
+            try
+            {
+                using (var powerShell = PowerShell.Create())
+                {
+                    foreach (ListViewItem item in lvDrives.Items)
+                    {
+                        if (ItemOk(item))
+                        {
+                            continue;
+                        }
+
+                        var script =
+                            $"New-SmbMapping -LocalPath {item.SubItems[1].Text} -RemotePath {item.SubItems[2].Text} -Persistent $True";
+
+                        powerShell.Commands.AddScript(script);
+                    }
+
+                    powerShell.Invoke();
+                }
+
+                RefreshDrives();
+
+                if (AllOk)
+                {
+                    AllowClose = true;
+                    Close();
+                }
+            }
+            catch
+            {
+                // ignored..
+            }
+        }
+
+
+        private void TmRefresh_Tick(object sender, EventArgs e)
+        {
+            tmRefresh.Enabled = false;
+
+            if (TryCountSeconds > CurrentCountMilliseconds / 1000)
+            {
+                AllowClose = true;
+                Close();
+                return;
+            }
+
+            UpdateMappedDrives();
+
+            CurrentCountMilliseconds += tmRefresh.Interval;
+
+            tmRefresh.Enabled = true;
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            RefreshDrives();
+            tmRefresh.Enabled = true;
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!AllowClose)
+            {
+                e.Cancel = true;
+                Hide();
+                return;
+            }
+            tmRefresh.Enabled = false;
+            Thread.Sleep(300);
+            RefreshWindowsExplorer();
+        }
+
+        private bool AllowClose { get; set; }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AllowClose = true;
+            Close();
+        }
+
+        /// <summary>
+        /// (C):: https://stackoverflow.com/questions/2488727/refresh-windows-explorer-in-win7
+        /// </summary>
+        private static void RefreshWindowsExplorer()
+        {
+            try
+            {
+                // based on http://stackoverflow.com/questions/2488727/refresh-windows-explorer-in-win7
+                Guid clsIdShellApplication = new Guid("13709620-C279-11CE-A49E-444553540000");
+                Type shellApplicationType = Type.GetTypeFromCLSID(clsIdShellApplication, true);
+
+                object shellApplication = Activator.CreateInstance(shellApplicationType);
+                object windows = shellApplicationType.InvokeMember("Windows",
+                    System.Reflection.BindingFlags.InvokeMethod, null, shellApplication, new object[] { });
+
+                Type windowsType = windows.GetType();
+                object count = windowsType.InvokeMember("Count", System.Reflection.BindingFlags.GetProperty, null,
+                    windows, null);
+                for (int i = 0; i < (int) count; i++)
+                {
+                    object item = windowsType.InvokeMember("Item", System.Reflection.BindingFlags.InvokeMethod, null,
+                        windows, new object[] {i});
+                    Type itemType = item.GetType();
+
+                    // only refresh windows explorers
+                    string itemName = (string) itemType.InvokeMember("Name", System.Reflection.BindingFlags.GetProperty,
+                        null, item, null);
+
+                    if (itemName == "Windows Explorer" || itemName == "File Explorer")
+                    {
+                        itemType.InvokeMember("Refresh", System.Reflection.BindingFlags.InvokeMethod, null, item, null);
+                    }
+                }
+            }
+            catch
+            {
+                // ignored..
+            }
+        }
+    }
+}
